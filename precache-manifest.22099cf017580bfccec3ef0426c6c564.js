67e7245b82f99243a7cd79be5dd2da215f06f972
mugiwara404 <aminugrahaewean@gmail.com>
2020-10-12 17:30:48 +0700

    Updates

---
+++ precache-manifest.22099cf017580bfccec3ef0426c6c564.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af2062600ac185668a7741398a77e27",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "db81dacfdb7d3d5d2977",
+    "url": "/covid19/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3c4ed4b856657b14d8ef",
+    "url": "/covid19/static/css/main.d7f2c6c1.chunk.css"
+  },
+  {
+    "revision": "db81dacfdb7d3d5d2977",
+    "url": "/covid19/static/js/2.54933112.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid19/static/js/2.54933112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c4ed4b856657b14d8ef",
+    "url": "/covid19/static/js/main.abc63252.chunk.js"
+  },
+  {
+    "revision": "636bdfdab0a46f674c1f",
+    "url": "/covid19/static/js/runtime-main.09a13a41.js"
+  },
+  {
+    "revision": "9395b06f5399a645414f13eebce59416",
+    "url": "/covid19/static/media/logoCovid.9395b06f.png"
+  },
+  {
+    "revision": "d6a62b4d746473d2b8b911a67bf4eda1",
+    "url": "/covid19/static/media/sad.d6a62b4d.png"
+  },
+  {
+    "revision": "9735ba05ccdd8199a105d01871e59023",
+    "url": "/covid19/static/media/sick.9735ba05.png"
+  }
+]);
